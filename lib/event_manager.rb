e7caf2163587f7b5df0bac8e65a75089cc96c8f6
erikbenton <erik.benton@hotmail.com>
2017-12-06 15:38:18 -0500

    adding personalized thank you and tracking of weekday and hour registration times

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,25 @@
 require 'erb'
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'date'
 
 def clean_zipcode(zipcode)
+	
 	zipcode = zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def clean_phone_number(phone_number)
+	phone_number = phone_number.to_s.split(/\(|\)|-|\.|\s/).join("")
+		if phone_number.length < 10 || phone_number.length > 11
+			phone_number = "BAD"
+		elsif phone_number.length == 11
+			if phone_number[0].to_s == "1"
+				phone_number = phone_number[1..10]
+			else
+				phone_number = "BAD"
+			end
+		end
+		phone_number
 end
 
 def legislators_by_zipcode(zipcode)
@@ -20,6 +36,26 @@ def legislators_by_zipcode(zipcode)
   end
 end
 
+def save_thank_you_letters(id, form_letter)
+	Dir.mkdir("../output") unless Dir.exist? "../output"
+
+	filename = "../output/thanks_#{id}.html"
+
+	File.open(filename, 'w') do |file|
+		file.puts form_letter
+	end
+end
+
+def registration_date_time(reg_date)
+
+	DateTime.strptime(reg_date.to_s, '%m/%d/%Y %H:%M')
+end
+
+def registration_weekday(reg_wday)
+
+	%w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday Sunday][reg_wday]
+end
+
 puts "EventManager Initialized!"
 
 template_letter = File.read("../form_letter.erb")
@@ -28,12 +64,16 @@ erb_template = ERB.new(template_letter)
 if File.exist? "../event_attendees.csv"
 	contents = CSV.open "../event_attendees.csv", headers: true, header_converters: :symbol
 	contents.each do |row|
+		id = row[0]
 		name = row[:first_name]
+		phone_number = clean_phone_number(row[:homephone])
+		reg_date_time = registration_date_time(row[:regdate])
+		reg_hour = reg_date_time.hour
+		reg_wday = registration_weekday(reg_date_time.wday)
 		zipcode = clean_zipcode(row[:zipcode])
 		legislators = legislators_by_zipcode(zipcode)
 		form_letter = erb_template.result(binding)
-		
-		puts form_letter
+		save_thank_you_letters(id, form_letter)
 	end
 end
 
